ce886edbad5d94882d25e9bafd80f6aab604fa8a
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2024-04-04 21:33:44 +0800

    feat: now allows libs that required to be installed automatically called and installed in the project's setup

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = "-e ."
+
+
+def get_requirements(path: str) -> List[str]:
+    """
+    this function will return the list of strings contain the requirement needed
+    """
+    requirements = []
+    with open(path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
 setup(
     name="end-to-end ml project",
     version="0.0.1",
